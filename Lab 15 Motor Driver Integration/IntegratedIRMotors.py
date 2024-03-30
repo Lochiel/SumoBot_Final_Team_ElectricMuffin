8e198db5c1c1d4e47b8241d4d2d4b72f6fe6dc8a
Cam Chalmers <Cam@BlueLochiel.com>
2024-03-29 23:26:01 -0600

    Updated with the IntegratdIRMotors.py written by Anusha and Melissa in class

--- Lab 15 Motor Driver Integration/IntegratedIRMotors.py
+++ Lab 15 Motor Driver Integration/IntegratedIRMotors.py
@@ -10,51 +10,104 @@ from ir_rx.print_error import print_error # for debugging
 def ir_callback(data, addr, _):
     print(f"Received NEC command! Data: 0x{data:02X}, Addr: 0x{addr:02X}")
 
+    # Each if-statement specifies a state that the motor will be at, based on which hex value is being transmitted
+
+    # Turns A motor off
+    if(data == 0xa0): 
+        Athrottle.duty_u16(PWM_MIN)
+        sleep(0.1)
+        Agear.value(0)
+
+    # Starts A motor forward 
+    elif(data == 0xa1):
+        Athrottle.duty_u16(PWM_MIN)
+        sleep(0.1)
+        Agear.value(0)
+        Athrottle.duty_u16(pwm_custom)
+        
+    # Starts A motor backwards 
+    elif(data == 0xa2):
+        Athrottle.duty_u16(PWM_MIN)
+        sleep(0.1)
+        Agear.value(1)
+        Athrottle.duty_u16(pwm_custom)
+        
+    # Turns B motor off
+    elif(data == 0xb0):
+        Bthrottle.duty_u16(PWM_MIN)
+        sleep(0.1)
+        Bgear.value(0)
+    
+    # Starts B motor forwards
+    elif(data == 0xb1):
+        Bthrottle.duty_u16(PWM_MIN)
+        sleep(0.1)
+        Bgear.value(0)
+        Bthrottle.duty_u16(pwm_custom)
+    
+    # Starts B motor backwards
+    elif(data == 0xb2):
+        Bthrottle.duty_u16(PWM_MIN)
+        sleep(0.1)
+        Bgear.value(1)
+        Bthrottle.duty_u16(pwm_custom)
+
+
 # Setup the IR receiver
 ir_pin = Pin(18, Pin.IN, Pin.PULL_UP) # Adjust the pin number based on your wiring
 ir_receiver = NEC_8(ir_pin, callback=ir_callback)
 # Optional: Use the print_error function for debugging
 ir_receiver.error_function(print_error)
 
-#Pin.value(x) evaluates x for truthiness. True sets Pin High. False sets Pin Low
-FWD = True
-REV = False
-
 PWM_MAX = 65535
 PWM_MIN = 0
-
 LED = Pin("LED", Pin.OUT)
+pwm_custom = int(65535/2)
 
-Aenbl = PWM(Pin(15), freq=2000, duty_u16=32768)
-Aphase = Pin(14, Pin.OUT)
+Bthrottle = PWM(Pin(20), freq=2000, duty_u16=32768)
+Bgear = Pin(19, Pin.OUT)
+Bthrottle.duty_u16(pwm_custom)
 
-Benbl = PWM(Pin(20), freq=2000, duty_u16=32768)
-Bphase = Pin(19, Pin.OUT)
-
-def ChangeGear(motor, gear_pin, gear):
-    motor.duty_u16(0)
-    sleep(2)
-    if type(gear) is bool:
-        gear_pin.value(gear)
-        LED.toggle()
-
-def ChangeSpeed(motor, speed):
-     speed_value = int( (speed/100) * PWM_MAX ) ## Convert speed to a percentage. Apply that to large
-     motor.duty_u16(speed_value)
+Athrottle = PWM(Pin(15), freq=2000, duty_u16=32768)
+Agear = Pin(14, Pin.OUT)
+Athrottle.duty_u16(pwm_custom)
 
 while True:
-    ChangeGear(Aenbl, Aphase, FWD)
-    ChangeSpeed(Aenbl, 50)
-    print("Motor A : Forward")
+    pass
+# while True: 
+#     if(addr == 0xa0):        
+#     if(addr == 0xa1):
+#     if(addr == 0xb0):
+#     if(addr == 0xb1):
 
-    ChangeGear(Benbl, Bphase, FWD)
-    ChangeSpeed(Benbl, 50)
-    print("Motor B : Forward")
+#     Bthrottle.duty_u16(0)
+#     sleep(2)
+#     print("Motor B : Forward")
+#     Bgear.value(0)
+#     LED.toggle()
+#     Bthrottle.duty_u16(pwm_custom)
+#     sleep(2)
 
-    ChangeGear(Aenbl, Aphase, REV)
-    ChangeSpeed(Aenbl, 50)
-    print("Motor A : Reverse")
+#     Bthrottle.duty_u16(0)
+#     sleep(.2)
+#     print("Motor B : Backward") 
+#     Bgear.toggle()
+#     LED.toggle()
+#     Bthrottle.duty_u16(pwm_custom)
+#     sleep(2)
 
-    ChangeGear(Benbl, Bphase, REV)
-    ChangeSpeed(Benbl, 50)
-    print("Motor B : Reverse")
+#     Athrottle.duty_u16(0)
+#     sleep(2)
+#     print("Motor A : Forward")
+#     Agear.value(0)
+#     LED.toggle()
+#     Bthrottle.duty_u16(pwm_custom)
+#     sleep(2)
+
+#     Athrottle.duty_u16(0)
+#     sleep(.2)
+#     print("Motor A : Backward") 
+#     Agear.toggle()
+#     LED.toggle()
+#     Athrottle.duty_u16(pwm_custom)
+#     sleep(2)
--- Lab 15 Motor Driver Integration/IntegratedIRMotors_v2.py
+++ Lab 15 Motor Driver Integration/IntegratedIRMotors_v2.py
@@ -1,7 +1,5 @@
-from Motor import Motor
-from machine import Pin
-from time import sleep_ms
-
+from machine import Pin, PWM
+from time import sleep
 from ir_rx.nec import NEC_8 # Use the NEC 8-bit class
 from ir_rx.print_error import print_error # for debugging
 # create PWM object from a pin and set the frequency = 20Hz of slice associated with pin 3
@@ -18,32 +16,45 @@ ir_receiver = NEC_8(ir_pin, callback=ir_callback)
 # Optional: Use the print_error function for debugging
 ir_receiver.error_function(print_error)
 
+#Pin.value(x) evaluates x for truthiness. True sets Pin High. False sets Pin Low
+FWD = True
+REV = False
+
+PWM_MAX = 65535
+PWM_MIN = 0
+
 LED = Pin("LED", Pin.OUT)
 
-MotorA = Motor(15,14)
-MotorB = Motor(20,19)
+Aenbl = PWM(Pin(15), freq=2000, duty_u16=32768)
+Aphase = Pin(14, Pin.OUT)
+
+Benbl = PWM(Pin(20), freq=2000, duty_u16=32768)
+Bphase = Pin(19, Pin.OUT)
+
+def ChangeGear(motor, gear_pin, gear):
+    motor.duty_u16(0)
+    sleep(2)
+    if type(gear) is bool:
+        gear_pin.value(gear)
+        LED.toggle()
+
+def ChangeSpeed(motor, speed):
+     speed_value = int( (speed/100) * PWM_MAX ) ## Convert speed to a percentage. Apply that to large
+     motor.duty_u16(speed_value)
 
 while True:
-    MotorA.gear(Motor.FWD)
-    MotorA.speed(50)
+    ChangeGear(Aenbl, Aphase, FWD)
+    ChangeSpeed(Aenbl, 50)
     print("Motor A : Forward")
-    LED.toggle()
 
-    MotorB.gear(Motor.FWD)
-    MotorB.speed(50)
+    ChangeGear(Benbl, Bphase, FWD)
+    ChangeSpeed(Benbl, 50)
     print("Motor B : Forward")
-    LED.toggle()
 
-    sleep_ms(3000)
-
-    MotorA.gear(Motor.REV)
-    MotorA.speed(50)
+    ChangeGear(Aenbl, Aphase, REV)
+    ChangeSpeed(Aenbl, 50)
     print("Motor A : Reverse")
-    LED.toggle()
 
-    MotorB.gear(Motor.REV)
-    MotorB.speed(50)
+    ChangeGear(Benbl, Bphase, REV)
+    ChangeSpeed(Benbl, 50)
     print("Motor B : Reverse")
-    LED.toggle()
-
-    sleep_ms(3000)
