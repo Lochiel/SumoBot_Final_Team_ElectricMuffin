898d6a4414a832cfb349001e27f5089897c0d79c
Cam Chalmers <Cam@BlueLochiel.com>
2024-04-21 13:08:25 -0600

    Started Tx Test Development

--- IR Protocal/ir_tx/test.py
+++ IR Protocal/ir_tx/test.py
@@ -1,6 +1,7 @@
 from slam import SLAM
 from nec import NEC
 from machine import Pin
+from time import sleep
 
 # indicator_led = Pin("LED", Pin.OUT)
 
@@ -10,6 +11,10 @@ FREQ_40 = 40_000
 FREQ_56 = 56_000
 
 pin = Pin("LED", Pin.OUT)
+pin_Tx_test = Pin(6,Pin.IN,Pin.PULL_UP)
+
+def Tx_test_pin():
+    return not( pin_Tx_test.value())
 
 def TxTimeCalculation(input):
     packet_list = input
@@ -30,3 +35,18 @@ printSummery(slam_test)
 nec_test = NEC(pin)
 nec_test.tx(0x05,0x0F,None)
 printSummery(nec_test)
+
+
+while True:
+    if not Tx_test_pin():
+        sleep(0.1)
+    for addr in range(0,0xF,):
+        if not Tx_test_pin():
+            break
+        for data in range(0,0xF):
+            print(f"Tx addr: {addr} data: {data}")
+            slam_test.transmit(addr,data)
+            sleep(0.1)
+            if not Tx_test_pin():
+                print("Stopping due to input")
+                break
